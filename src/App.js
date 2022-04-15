fe072ad3fc06352cdabc46ad8c27b5bd5f81fa59
jasim <jasimihsan2@gmail.com>
2022-04-15 19:57:09 +0530

    Added a global state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import Header from './components/Header';
 import Tasks from './components/Task';
+import {useState} from 'react';
 
-function App() {
-    
+const App = () =>{
+    const[tasks,setTask] = useState([
+
+      {
+          id : 1,
+          text:'Doctor appoinment',
+          dat:'Feb 5th at 2:30pm',
+          reminder: true,
+      },
+      {      
+          id : 2,
+          text:'Ea proident mollitint',
+          dat:'Feb 7th at 2:40pm',
+          reminder: true,
+      },
+      {
+      id : 3,
+      text:'Anim magna proident',
+      dat:'Feb 6th at 2:30pm',
+      reminder: false,
+      },
+  
+  ])
+  
 
   return (
     <div className="container">
 
     <Header  />
-    <Tasks />
+    <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,31 +1,7 @@
-import {useState} from 'react';
 
 
-
-const Tasks=() =>
+const Tasks=({tasks}) =>
 {
-const[tasks,setTask] = useState([
-
-    {
-        id : 1,
-        text:'Doctor appoinment',
-        dat:'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {      
-        id : 2,
-        text:'Ea proident mollitint',
-        dat:'Feb 7th at 2:40pm',
-        reminder: true,
-    },
-    {
-    id : 3,
-    text:'Anim magna proident',
-    dat:'Feb 6th at 2:30pm',
-    reminder: false,
-    }
-
-])
 
     return(
         <>
